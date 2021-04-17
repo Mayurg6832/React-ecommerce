57cc0c4c0f36d36477cd51a8a7a0d5a9b84ad22c
Mayur Goyal <Mayurg6832.mg@gmail.com>
2021-04-18 02:27:43 +0530

    integrated firebase into the application, created sign in page with sign in component and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBPD1enEw9RWDaD7PDOnc7eOF4c6gljOl4",
+  authDomain: "react-ecommerce-db-68e15.firebaseapp.com",
+  projectId: "react-ecommerce-db-68e15",
+  storageBucket: "react-ecommerce-db-68e15.appspot.com",
+  messagingSenderId: "749967497369",
+  appId: "1:749967497369:web:7ee7753cd6b6528c2b0008",
+  measurementId: "G-28R0E8DMPY",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
